586dffa05f5cad8ba9962d7c5372cac103b05ffe
Cody Stewart <cjstewart88@gmail.com>
2012-01-25 13:59:08 -0500

    Cleaning some stuff up, removed champ and item shit, accounted for new counter-pick feature on .com, and .... nvm thats it.

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-var http    = require("http");
-var irc     = require("./irc.js");
-var $       = require('jquery'); 
+var http  = require("http");
+var irc   = require("./lib/irc/irc.js");
 
-var server  = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    "Content-Type":                 "application/json",
-    "Access-Control-Allow-Origin":  "*"
-  });
-}).listen(process.env.PORT || 3000);
+var the_bot           = null;
+var bot_disconnected  = false;
 
-var the_bot = new irc.Client("irc.quakenet.org", "soloqueue", { port: 6667, channels: ["#soloqueue", "#reddit.lol"] });
-var bot_disconnected = false;
-setup_bot_listeners(the_bot);
+start_bot();
+
+function start_bot () {
+  the_bot = new irc.Client("irc.freenode.net", "soloqueue", { port: 6667, channels: ["#mjc"] });
+  bot_disconnected = false;
+  setup_bot_listeners(the_bot);  
+}
 
 var statusCheck = setInterval(function () {
   if (bot_disconnected) {
-    bot_disconnected = false;
-    the_bot = new irc.Client("irc.quakenet.org", "soloqueue", { port: 6667, channels: ["#soloqueue", "#reddit.lol"] });
-    setup_bot_listeners(the_bot);
+    start_bot();
   }
 }, 2000);
 
@@ -65,91 +62,58 @@ function handle_data (data, command, from) {
   
   switch (command) {
     case "counters":
-      the_bot.say(from, the_data.data[0][0] + " counters: " + (the_data.data[0][1] != null ? the_data.data[0][1].counters.join(", ") : "n/a") + " [More on: http://www.soloqueue.com/#" + escape(the_data.data[0][0]) + "]");
+      var derived_from_forums_tmp = the_data.data[0][1].counters.curated;
+      var community_voted_tmp     = the_data.data[0][1].counters.community;
+      var derived_from_forums     = "";
+      var community_voted         = "";
+      
+      if (derived_from_forums_tmp != null) {
+        derived_from_forums = derived_from_forums_tmp.join(",").replace(/,/g, ", ");
+      }
+      else {
+        derived_from_forums = "n/a";
+      }
+      
+      if (community_voted_tmp != null) {
+        if (community_voted_tmp[0] != null) community_voted += community_voted_tmp[0][0] + (community_voted_tmp[0][1] != "" ? " (" + community_voted_tmp[0][1] + ")" : "");
+        if (community_voted_tmp[1] != null) community_voted += ", " + community_voted_tmp[1][0] + (community_voted_tmp[1][1] != "" ? " (" + community_voted_tmp[1][1] + ")" : "");
+        if (community_voted_tmp[2] != null) community_voted += ", " + community_voted_tmp[2][0] + (community_voted_tmp[2][1] != "" ? " (" + community_voted_tmp[2][1] + ")" : "");
+      }
+      else {
+        community_voted = "n/a";
+      }
+      
+      the_bot.say(from, the_data.data[0][0] + " counters: [Derived from Forums: " + derived_from_forums + "] [Community Voted: " + community_voted + "] [More on: http://www.soloqueue.com/#" + escape(the_data.data[0][0]) + "]");
                                
       break;
-    case "champ":
-      var found_champ = false;
-      var html;
-      var champ_name  = "";
-      var response    = "";
-      
-      $.each(the_data.data, function () {
-        html = this[1].wiki;
-        
-        if (!found_champ && $(html).size() != 1) {
-          found_champ = true;
-          
-          champ_name = this[0];
-          
-          $.each($($(html).find("tr")[1]).find("td"), function () {
-            response += " " + $(this).text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $(this).next().text().replace(/(\r\n|\n|\r)/gm, "");
-          });
-        }
-      });
-      
-      if (found_champ)  the_bot.say(from, "Champion Name: " + champ_name + response + " [More on: http://www.soloqueue.com/#" + escape(champ_name) + "]");
-      else              the_bot.say(from, "Sorry, the champion you requested data on couldn't be found.");
-      
-      break;
-    case "item":
-      var found_item  = false;
-      var html;
-      var item_name   = "";
-      var response    = "";
-      
-      $.each(the_data.data, function () {
-        html = this[1].wiki;
-        
-        if (!found_item && $(html).size() == 1) {
-          found_item  = true;
-          
-          item_name   = this[0];
-          
-          response += " " + $($(html).find("tr")[2]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[2]).find("td").text().replace(/(\r\n|\n|\r)/gm, "");
-          response += " " + $($(html).find("tr")[3]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[3]).find("td").text().replace(/(\r\n|\n|\r)/gm, "");
-          response += " " + $($(html).find("tr")[4]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[4]).find("td").text().replace(/(\r\n|\n|\r)/gm, "");
-          response += " " + $($(html).find("tr")[5]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[5]).find("td").text().replace(/(\r\n|\n|\r)/gm, "");
-          response += " " + $($(html).find("tr")[6]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[6]).find("td").text().replace(/(\r\n|\n|\r)/gm, "");
-          response += " " + $($(html).find("tr")[7]).find("th").text().replace(/(\r\n|\n|\r)/gm, "") + ": " + $($(html).find("tr")[7]).find("td").text().replace(/(\r\n|\n|\r)/gm, ""); 
-        }
-      });
-      
-      if (found_item) the_bot.say(from, "Item Name: " + item_name + response + " [More on: http://www.soloqueue.com/#" + escape(item_name) + "]");
-      else            the_bot.say(from, "Sorry, the item you requested data on couldn't be found, maybe try searching on http://www.soloqueue.com/");
-      
-      break;
+    default:
+      the_bot.say(from, "Looks like something went wrong, try again and if this keeps happening contact");
   }
 }
 
 function setup_bot_listeners (the_bot) {
   the_bot.addListener("message", function (from, channel, msg) {
-    if (from == "jermenkoo") return; // REALLY REALLY LAME BLACKLIST ATTEMPT O_o
-    
-    var is_pm = (channel.indexOf("#") == 0 ? false : true);
-    
+    var is_pm   = (channel.indexOf("#") == 0 ? false : true);
     var message = msg.replace(/^\s\s*/, '').replace(/\s\s*$/, '').split(" ");
         
     if (!is_pm && message[0].toLowerCase() != "!sq") return;
 
     var command = (is_pm ? message[0] : message[1]);
-    
-    if (command == "help") {
-      the_bot.say(from, "The soloqueue bot is a service brought to you by, http://www.soloqueue.com, a League of Legends information hub.");
-      the_bot.say(from, "Commands");
-      the_bot.say(from, "--------");
-      the_bot.say(from, "Item Search: '!sq item ITEM NAME'");
-      the_bot.say(from, "Champ Search: '!sq champ CHAMP NAME'");
-      the_bot.say(from, "Champ Counters: '!sq counters CHAMP NAME'");
-      return;
-    }
-    
-    if (command == "counters" || command == "champ" || command == "item") {
-      if ((is_pm ? message[1] : message[2]) == null) the_bot.say(from, "Please complete the command. Ex: Counters '!sq counters Teemo' - Champ Info '!sq champ Alistar' - Item Info '!sq item Doran'");
-      else get_data(search_term(message), command, (is_pm ? from : channel));
-    }
-    else {
-      the_bot.say((is_pm ? from : channel), "Sorry, " + from + " I dont recognize that command.");
+    switch (command) {
+      case "help":
+        the_bot.say(from, "The soloqueue bot is a service brought to you by, http://www.soloqueue.com, a League of Legends information hub.");
+        the_bot.say(from, "Commands");
+        the_bot.say(from, "--------");
+        the_bot.say(from, "Champ Counters: '!sq counters CHAMP NAME'");
+        
+        break;
+      case "counters":
+        if ((is_pm ? message[1] : message[2]) == null) the_bot.say(from, "Please complete the command. Ex: Counters '!sq counters Teemo'");
+        else get_data(search_term(message), command, (is_pm ? from : channel));
+        
+        break;
+      default:
+        the_bot.say((is_pm ? from : channel), "Sorry, " + from + " I dont recognize that command.");
     }
   });
 
